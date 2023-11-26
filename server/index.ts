11a67a682f5399cd967015d7b856a6720d3699e4
faizkhan12 <faizk6797@gmail.com>
2023-11-26 18:53:53 +0530

    Add cors

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,7 @@ import express from "express";
 import next, { NextApiHandler } from "next";
 import { Server } from "socket.io";
 import { v4 } from "uuid";
+import cors from 'cors'
 
 const port = parseInt(process.env.PORT || "3001", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -14,6 +15,8 @@ const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(async () => {
   const app = express();
+  app.use(cors());
+
   const server = createServer(app);
 
   const io = new Server<ClientToServerEvents, ServerToClientEvents>(server);
